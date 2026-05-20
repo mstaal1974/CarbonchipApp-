370284adaf4b1b181f1031a6af8c845847ba6da7
Mick Staal <87622017+mstaal1974@users.noreply.github.com>
2026-05-21 09:48:57 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'carbonchip-bi-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/assets/index-DrqCAZCV.js', '/assets/index-B7gFRBOR.css'];
+const CACHE = 'carbonchip-bi-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/assets/index-B_oHOQU-.js', '/assets/index-CCE2PNBq.css'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
